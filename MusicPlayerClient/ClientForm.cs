fbe927b8dc0a6b1ebf881c95084bfcefa0837e75
jeiea <solum5013@gmail.com>
2017-04-23 18:59:06 +0900

    Fix false trial: Use force stream close

--- MusicPlayerClient/ClientForm.cs
+++ MusicPlayerClient/ClientForm.cs
@@ -84,28 +84,22 @@ namespace MusicPlayerClient
     async void TcpWorker(IPEndPoint addr)
     {
       var client = new TcpClient();
-      Task<object> readTask = null;
       try
       {
         await client.ConnectAsync(addr.Address, addr.Port);
 
         Invoke(new Action(ReflectConnection));
         var stream = client.GetStream();
-        Task cancelTask = WorkerAbort.Token.GetHold();
-        readTask = stream.ReadObjAsync();
-        while (true)
+        WorkerAbort.Token.Register(() => { stream.Close(); });
+        while (!WorkerAbort.IsCancellationRequested)
         {
-          await Task.WhenAny(readTask, cancelTask);
-          if (WorkerAbort.IsCancellationRequested) break;
-          if (!readTask.IsCompleted) break;
-          var metas = readTask.Result as TrackMetadata[];
+          var metas = await stream.ReadObjAsync() as TrackMetadata[];
           var items = metas.Select(x => new ListViewItem(x.ListItem)).ToArray();
           Invoke(new Action(() =>
           {
             lvRemoteList.Items.Clear();
             lvRemoteList.Items.AddRange(items);
           }));
-          readTask = stream.ReadObjAsync();
         }
       }
       catch (Exception e) {
